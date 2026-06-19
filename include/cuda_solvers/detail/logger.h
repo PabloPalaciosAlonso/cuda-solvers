7568310831b76fb2536b84374d4780ded0fdb24c
PabloPalaciosAlonso <pablo.pal@hotmail.com>
2026-06-19 10:43:58 +0200

    Change colors

--- include/cuda_solvers/detail/logger.h
+++ include/cuda_solvers/detail/logger.h
@@ -6,13 +6,13 @@ inline bool stderr_is_terminal() {
   return isatty(fileno(stderr));
 }
 
-#define GREEN  (stderr_is_terminal() ? "\033[32m" : "")
-#define YELLOW (stderr_is_terminal() ? "\033[33m" : "")
-#define RED    (stderr_is_terminal() ? "\033[31m" : "")
+#define GREEN  (stderr_is_terminal() ? "\033[92m" : "")
+#define YELLOW (stderr_is_terminal() ? "\033[34m" : "")
+#define RED    (stderr_is_terminal() ? "\033[91m" : "")
 #define RESET  (stderr_is_terminal() ? "\033[0m"  : "")
 
 #define LOG_INFO(msg) \
-  std::cerr << GREEN << "[INFO]" << RESET << " " << msg << '\n'
+  std::cerr << GREEN << "[MESSAGE]" << RESET << " " << msg << '\n'
 
 #define LOG_WARN(msg) \
   std::cerr << YELLOW << "[WARNING]" << RESET << " " << msg << '\n'
